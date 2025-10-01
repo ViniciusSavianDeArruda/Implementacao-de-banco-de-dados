1d20f10cc8d7f4dd0dc2cbd8f5f70f910093a6bb
Vinicius Arruda <arruda.vinicius@ufn.edu.br>
2025-09-30 21:54:12 -0300

    Add files via upload

--- Aulas Implementacao de banco de dados/Aula 30-09.sql
+++ Aulas Implementacao de banco de dados/Aula 30-09.sql
@@ -1,201 +1,200 @@
+-- Aula dia 30-09-2025
+-- Correcao Prova e transacoes
 
-CREATE DATABASE EMPRESA;
-GO
-	
-USE EMPRESA;
-GO
-	
---Criação da tabela FUNCIONARIO
-CREATE TABLE FUNCIONARIO (
-	Pnome VARCHAR(15) NOT NULL,
-    Minicial CHAR,
-    Unome VARCHAR(15) NOT NULL,
-    Cpf CHAR(11),
-    Datanasc DATE,
-    Endereco VARCHAR(255),
-    Sexo CHAR,
-    Salario DECIMAL(10,2),
-    Cpf_supervisor CHAR(11),
-    Dnr INT,
-    PRIMARY KEY (Cpf),
-    FOREIGN KEY (Cpf_supervisor) REFERENCES FUNCIONARIO(Cpf)
-);
-GO
+-- 7) 
+SELECT 
+	A.Nome AS 'Alunos',
+	D.Nome_disciplina AS 'Disciplina',
+	HE.Nota AS 'Nota'
+FROM ALUNO AS A
+INNER JOIN HISTORICO_ESCOLAR AS HE ON A.Numero_aluno = HE.Numero_aluno
+INNER JOIN TURMA AS T ON T.Identificacao_turma = HE.Identificacao_turma
+INNER JOIN DISCIPLINA AS D ON D.Numero_disciplina = T.Numero_disciplina
+WHERE D.Nome_disciplina = 'Banco de dados';
 
---Tablea de DEPARTAMENTO
-CREATE TABLE DEPARTAMENTO (
-	Dnome VARCHAR(15) NOT NULL,
-    Dnumero INT,
-    Cpf_gerente CHAR(11),
-    Data_inicio_gerente DATE,
-    PRIMARY KEY (Dnumero),
-    UNIQUE (Dnome),
-    FOREIGN KEY (Cpf_gerente) REFERENCES FUNCIONARIO(CPF)
-);
 
---Adiconando restição referencial em FUNCIONARO de DEPARTAMENTO
---Criando uma ALTERAÇÃO de Tabela
-ALTER TABLE FUNCIONARIO
-ADD CONSTRAINT Dnr
-FOREIGN KEY (Dnr) REFERENCES DEPARTAMENTO (Dnumero);
+-- 8)
+SELECT 
+	D.Nome_disciplina,
+	DPR.Nome_disciplina
+FROM DISCIPLINA AS D
+INNER JOIN PRE_REQUISITO AS PR 
+	ON D.Numero_disciplina = PR.Numero_disciplina
+INNER JOIN DISCIPLINA AS DPR
+	ON DPR.Numero_disciplina = PR.Numero_pre_requisito
 
---Criação da tabela de LOCALIZACAO_DEP
-CREATE TABLE LOCALIZACAO_DEP (
-	Dnumero INT NOT NULL,
-	Dlocal VARCHAR (15) NOT NULL,
-    PRIMARY KEY (Dnumero, Dlocal),
-    FOREIGN KEY (Dnumero) REFERENCES DEPARTAMENTO (Dnumero)
-);
+-- 9)
+SELECT 
+	D.Nome_disciplina,
+	T.Semestre,
+	T.Ano,
+	HE.Nota
+FROM ALUNO AS A
+INNER JOIN HISTORICO_ESCOLAR AS HE ON A.Numero_aluno = HE.Numero_aluno
+INNER JOIN TURMA AS T ON T.Identificacao_turma = HE.Identificacao_turma
+INNER JOIN DISCIPLINA AS D ON D.Numero_disciplina = T.Numero_disciplina
+WHERE A.Nome = 'Silva';
 
---Criacao da tabela PROJETO
-CREATE TABLE PROJETO(
-	Projnome VARCHAR (15) NOT NULL,
-	Projnumero INT NOT NULL,
-    Projlocal VARCHAR(15),
-    Dnum INT,
-    PRIMARY KEY (Projnumero),
-    UNIQUE (Projnome),
-    FOREIGN KEY (Dnum) REFERENCES DEPARTAMENTO (Dnumero)
-);
-
---Criação da tabela TRABALHA_EM
-CREATE TABLE TRABALHA_EM(
-	Fcpf CHAR(11) NOT NULL,
-    Pnr INT NOT NULL,
-    Horas DECIMAL (3,1) NOT NULL,
-    PRIMARy KEY (Fcpf, Pnr),
-    FOREIGN KEY (Fcpf) REFERENCES FUNCIONARIO (Cpf),
-    FOREIGN KEY (Pnr) REFERENCES PROJETO(Projnumero)
-);
-
---Criação da tabela DEPENDENTE sem seleção do esquema
-CREATE TABLE DEPENDENTE(
-	Fcpf CHAR(11) NOT NULL,
-    Nome_dependente VARCHAR(15) NOT NULL,
-    Sexo CHAR,
-    Datanasc DATE,
-    Parentesco VARCHAR(8),
-    PRIMARY KEY (Fcpf, Nome_dependente),
-    FOREIGN KEY (Fcpf) REFERENCES FUNCIONARIO(Cpf)
-);
-
---Inserindo valores no Departamento
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('Pesquisa', 5);
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('Administração', 4);
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('Matriz', 1);
-SELECT * FROM DEPARTAMENTO;
-
---Inserindo funcionarios com cargo de gerencia DATE AAAA-MM-DD
-INSERT INTO FUNCIONARIO VALUES ( 'Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto, 35, São Paulo, SP', 'M', 55000, NULL , 1 );
-INSERT INTO FUNCIONARIO VALUES ( 'Jennifer', 'S', 'Souza', '98765432168', '1941-06-20', 'Av Arthur de Lima, 54, Santo André, SP', 'F', 43000, '88866555576' , 4 );
-INSERT INTO FUNCIONARIO VALUES ( 'Fernando', 'T', 'Wong', '33344555587', '1955-12-08', 'Rua da Lapa, 34, São Paulo, SP', 'M', 40000, '88866555576' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'João', 'B', 'Silva', '12345678966', '1965-01-09', 'Rua das Flores, 751, São Paulo, SP', 'M', 30000, '33344555587' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'Alice', 'J', 'Zelaya', '99988777767', '1968-01-19', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, '98765432168' , 4 );
-INSERT INTO FUNCIONARIO VALUES ( 'Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15', 'Rua Rebouças, 65, Piracicaba, SP', 'M', 38000, '33344555587' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'Joice', 'A', 'Leite', '45345345376', '1972-07-31', 'Av. Lucas Obes, 74, São Paulo, SP', 'F', 25000, '33344555587' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'André', 'E', 'Brito', '98798798733', '1969-03-29', 'Rua Timbira, 35, São Paulo, SP', 'M', 25000, '98765432168' , 4 );
-
---Corrigindo erro de inserção
-UPDATE FUNCIONARIO
-SET Endereco = 'Rua Reboucas, 65, Piracicaba, SP'
-WHERE Cpf = '66688444476';
-
---Recupera todas as informações de funcionários
-SELECT * FROM FUNCIONARIO;
-
---Finzalindo o preenchimento da tabela DEPTARTAMENTO
-UPDATE DEPARTAMENTO
-SET Cpf_gerente = '33344555587', Data_inicio_gerente = '1988-05-22'
-WHERE Dnumero = 5;
-UPDATE DEPARTAMENTO
-SET Cpf_gerente = '98765432168', Data_inicio_gerente = '1995-01-01'
-WHERE Dnumero = 4;
-UPDATE DEPARTAMENTO
-SET Cpf_gerente = '88866555576', Data_inicio_gerente = '1981-06-19'
-WHERE Dnumero = 1;
---Recuperar todas as informações de departamento
-SELECT * FROM DEPARTAMENTO;
-
---Prrencher a tabela LOCALIZACAO_DEP
-INSERT INTO LOCALIZACAO_DEP VALUES (1, 'São Paulo');
-INSERT INTO LOCALIZACAO_DEP VALUES (4, 'Mauá');
-INSERT INTO LOCALIZACAO_DEP VALUES (5, 'Santo André');
-INSERT INTO LOCALIZACAO_DEP VALUES (5, 'Itu');
-INSERT INTO LOCALIZACAO_DEP VALUES (5, 'São Paulo');
---Recuperando informações de LOCALIZACAO_DEP
-SELECT * FROM LOCALIZACAO_DEP;
-
---Preenchendo a table PROJETO
-INSERT INTO PROJETO VALUES ('ProdutoX', 1, 'Santo André', 5);
-INSERT INTO PROJETO VALUES ('ProdutoY', 2, 'Itu', 5);
-INSERT INTO PROJETO VALUES ('ProdutoZ', 3, 'São Paulo', 5);
-INSERT INTO PROJETO VALUES ('Informatização', 10, 'Mauá', 4);
-INSERT INTO PROJETO VALUES ('Reorganização', 20, 'São Paulo', 1);
-INSERT INTO PROJETO VALUES ('Novosbenefícios', 30, 'Mauá', 4);
---Recuperando informações de PROJETO
-SELECT * FROM PROJETO;
-
---Preenchento TRABALHA_EM
-INSERT INTO TRABALHA_EM VALUES ('12345678966',1,32.5);
-INSERT INTO TRABALHA_EM VALUES ('12345678966',2,7.5);
-INSERT INTO TRABALHA_EM VALUES ('66688444476',3,40);
-INSERT INTO TRABALHA_EM VALUES ('45345345376',1,20);
-INSERT INTO TRABALHA_EM VALUES ('45345345376',2,20);
-INSERT INTO TRABALHA_EM VALUES ('33344555587',2,10);
-INSERT INTO TRABALHA_EM VALUES ('33344555587',3,10);
-INSERT INTO TRABALHA_EM VALUES ('33344555587',10,10);
-INSERT INTO TRABALHA_EM VALUES ('33344555587',20,10);
-INSERT INTO TRABALHA_EM VALUES ('99988777767',10,10);
-INSERT INTO TRABALHA_EM VALUES ('99988777767',30,30);
-INSERT INTO TRABALHA_EM VALUES ('98798798733',10,35);
-INSERT INTO TRABALHA_EM VALUES ('98798798733',30,5);
-INSERT INTO TRABALHA_EM VALUES ('98765432168',30,20);
-INSERT INTO TRABALHA_EM VALUES ('98765432168',20,15);
-
---Preenchendo a tabela de DEPENDENTES
-INSERT INTO DEPENDENTE VALUES ('33344555587', 'Alicia', 'F', '1986-04-05', 'Filha');
-INSERT INTO DEPENDENTE VALUES ('33344555587', 'Tiago', 'M', '1983-10-25', 'Filh0');
-INSERT INTO DEPENDENTE VALUES ('33344555587', 'Janaina', 'F', '1958-05-03', 'Eposa');
-INSERT INTO DEPENDENTE VALUES ('98765432168', 'Antonio', 'M', '1942-02-28', 'Marido');
-INSERT INTO DEPENDENTE VALUES ('12345678966', 'Michael', 'M', '1988-01-04', 'Filho');
-INSERT INTO DEPENDENTE VALUES ('12345678966', 'Alicia', 'F', '1988-12-30', 'Filha');
-INSERT INTO DEPENDENTE VALUES ('12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
-
--- Estes comando para a aula de IF/ELSE
--- Adicionando a coluna Bonus
-ALTER TABLE FUNCIONARIO
-ADD Bonus DECIMAL(10, 2) NULL;
 GO
 
--- Adicionando a coluna Data_Admissao
-ALTER TABLE FUNCIONARIO
-ADD Data_Admissao DATE NULL;
+-- 10)
+CREATE OR ALTER FUNCTION fn_statusAprvacaoAluno
+(
+	@NomeAluno VARCHAR(50),
+	@NomeDisciplina VARCHAR(50)
+)
+RETURNS VARCHAR(20)
+AS
+BEGIN
+	DECLARE @nota CHAR(1);
+	DECLARE @status VARCHAR(20)
+
+	SELECT @nota = HE.Nota
+FROM ALUNO AS A
+INNER JOIN HISTORICO_ESCOLAR AS HE ON A.Numero_aluno = HE.Numero_aluno
+INNER JOIN TURMA AS T ON T.Identificacao_turma = HE.Identificacao_turma
+INNER JOIN DISCIPLINA AS D ON D.Numero_disciplina = T.Numero_disciplina
+WHERE A.Nome = @NomeAluno AND D.Nome_disciplina = @NomeDisciplina
+
+IF @nota = 'A' OR @nota = 'B'
+	SET @status = 'Aprovado'
+ELSE IF @nota = 'C'
+	SET @status = 'Em Recuperacao'
+ELSE IF @nota = 'F'
+	SET @status = 'Reprovado'
+ELSE 
+	SET @status = 'Sem registro'
+RETURN @status;
+END;
 GO
+SELECT dbo.fn_statusAprvacaoAluno ('Silva', 'Matematica discreta') AS 'StatusAprovacao';
 
 
--- Inserindo novos registros com valores para as colunas Bonus e Data_Admissao
-INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr, Data_Admissao, Bonus)
-VALUES
-('Carlos', 'A', 'Silva', '98765432100', '1985-04-12', 'Rua A, 123', 'M', 4500.00, NULL, 1, '2023-03-01', 1000.00),
-('Ana', 'B', 'Sousa', '98765432200', '1990-06-23', 'Rua B, 456', 'F', 6500.00, '98765432100', 1, '2022-01-15', 0.00),
-('Maria', 'C', 'Oliveira', '98765432300', '1978-09-17', 'Rua C, 789', 'F', 7500.00, '98765432200', 5, '2024-02-01', 1500.00),
-('Paulo', 'D', 'Silva', '98765432400', '1982-11-05', 'Rua D, 101', 'M', 7000.00, '98765432300', 4, '2024-05-15', 500.00);
+-- 11)
+
+
+
+--12)
 GO
+CREATE OR ALTER FUNCTION fn_statusLocacao(@IdTurma INT)
+RETURNS VARCHAR (30);
+AS
+BEGIN
+	DECLARE @qtd INT;
+	DECLARE @status VARCHAR(30);
+
+	SELECT @qtd = COUNT(*)
+	FROM HISTORICO_ESCOLAR
+	WHERE Identificacao_turma = @IdTurma;
+
+	IF(@qtd > = 5)
+		SET @status = 'Completamente Lotada'
+	ELSE IF( @qtd BETWEEN 3 AND 4)
+		SET @status = 'Quase cheio'
+	ELSE 
+		SET @status = 'Com vagas'
+	RETURN @status;
+END;
+GO
+SELECT DISTINCT
+	T.Identificacao_turma,
+	D.Nome_disciplina,
+	dbo.fn_statusLotacao(T.identificacao_turma) AS 'Status'
+FROM HISTORICO_ESCOLAR AS HE
+JOIN TURMA AS T ON HE.Identificacao_turma = T.Identificacao_turma
+JOIN DISCIPLINA AS D ON T.Numero_disciplina = D.Numero_disciplina
+
+-- 13)
+GO
+CREATE OR ALTER PROCEDURE usp_CalcularIdade
+	@NumeroAluno INT
+AS 
+BEGIN
+	DECLARE @dataAtual DATE;
+	DECLARE @dataNasc DATE;
+	DECLARE @Idade INT;
+
+	SET @dataAtual = GETDATE();
+
+	SELECT @dataNasc = A.Data_Nascimento
+	FROM ALUNO AS A
+	WHERE A.Numero_aluno = @NumeroAluno;
+
+	SET @Idade = DATEDIFF(YEAR @dataNasc, @dataAtual)
+
+	IF(MONTH(@dataNasc) > MONTH(@dataAtual)
+		OR (MONTH(@dataNasc) = MONTH(@dataAtual) AND DAY(@dataNasc) > DAY (@dataAtual)))
+		SET @Idade = @Idade -1;
 
 
---Novos funcionario e departaemtneo para Aula 03
+-- CONTINUACAO AULA, TRANSACOES
+/* 
+O que são Transações no Banco de Dados?
+
+Uma transação é um conjunto de operações SQL (como INSERT, UPDATE, DELETE) que são executadas como uma única unidade lógica. 
+Ou tudo é feito com sucesso, ou nada é feito.
+======================================================
+Conceito ACID (quatro pilares das transações)
+ACID garante que as transações sejam seguras e confiáveis:
+
+Atomicidade
+Tudo ou nada.
+Se uma parte da transação falha, tudo é desfeito (rollback).
+Garante que não fiquem alterações parciais no banco.
+=================================================
+Consistência
+Os dados devem sempre seguir as regras e restrições do banco (como chaves estrangeiras, tipos de dados, etc).
+Após a transação, os dados ainda devem estar válidos e coerentes.
+===================================================
+Isolamento
+Transações que ocorrem ao mesmo tempo não interferem umas nas outras.
+O que uma transação faz não pode ser visto por outra até ser finalizada.
+====================================================
+Durabilidade
+Depois de um COMMIT, os dados ficam salvos permanentemente, mesmo se o sistema cair logo em seguida.
+O banco garante que as alterações foram gravadas no disco.
+=========================================================
+TRANSACTION = É utilizado para gerenciar uma sequência de operações (transações)no banco de dados, garantindo que essas operações sejam
+executadas de maneira segura e consistente.
+===========================================================
+Comandos Relacionados a Transações:
+BEGIN TRANSACTION: Inicia uma nova transação.
+=====================================================
+COMMIT TRANSACTION: Confirma a transação, aplicando permanentemente todas as operações feitas no banco de dados.
+=================================================================
+ROLLBACK TRANSACTION: Desfaz todas as operações realizadas desde o início da transação.
+===================================================================
+SAVEPOINT: Define um ponto dentro de uma transação para permitir um rollback parcial, até esse ponto
+*/
+
+--Exemplo
+BEGIN TRANSACTION;
+
+DECLARE @Erro INT;
+
+-- Insira um novo funcionário
 INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
-VALUES ('Carlos', 'M', 'Ferreira', '12312312311', '1980-02-15', 'Av. Paulista, 1000, São Paulo, SP', 'M', 45000, NULL, NULL),
-('Mariana', 'L', 'Gomes', '32132132122', '1985-06-22', 'Rua das Acácias, 500, Rio de Janeiro, RJ', 'F', 42000, NULL, NULL),
-('Pedro', 'A', 'Silva', '65465465433', '1990-11-10', 'Rua da Praia, 200, Salvador, BA', 'M', 47000, NULL, NULL);
-GO
+VALUES ('Carlos', 'M', 'Almeida', '98765432100', '1991-07-23', 'Av. Brasil, 500', 'M', 4500, NULL, 2);
 
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
-VALUES ('Vendas', 6),
-('RH', 7),
-('TI', 8);
-GO
+SET @Erro = @@ERROR;
 
+-- Insira um novo departamento
+INSERT INTO DEPARTAMENTO (Dnumero, Dnome, Cpf_gerente, Data_inicio_gerente)
+VALUES (10, 'Marketing', '98765432100', '2023-09-29');
+
+SET @Erro = @Erro + @@ERROR;
+
+-- Verifique se houve erro
+IF @@ERROR <> 0 
+BEGIN
+    ROLLBACK TRANSACTION;
+    PRINT 'Erro detectado. Transação revertida.';
+END
+ELSE
+BEGIN
+    COMMIT TRANSACTION;
+    PRINT 'Transação concluída com sucesso.';
+END;
 
 --Crie uma transação que atualize o salário de todos os funcionários de um determinado departamento.
 --Se a atualização de qualquer funcionário falhar, reverta todas as alterações.
